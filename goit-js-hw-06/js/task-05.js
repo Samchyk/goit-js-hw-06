3f3df6497f555d638e1b1603760e84b493bb7d5a
Samchyk <92978942+Samchyk@users.noreply.github.com>
2022-04-26 19:29:59 +0300

    Delete goit-js-hw-06 directory

--- goit-js-hw-06/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const inputElement = document.querySelector('input#name-input');
-const spanElementH1 = document.querySelector('span#name-output');
-
-inputElement.addEventListener('input', () => {
-   spanElementH1.innerHTML = inputElement.value;
-   if (!inputElement.value) {
-      spanElementH1.innerHTML = 'Anonymous';
-   }
-});
--- goit-js-hw-06/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputElement = document.querySelector('#font-size-control');
-const spanElement = document.querySelector('#text');
-
-inputElement.addEventListener('input', () => {
-   spanElement.style.fontSize = inputElement.value+"px";
-});
